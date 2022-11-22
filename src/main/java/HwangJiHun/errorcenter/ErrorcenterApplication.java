04a0558b5adfa7d976bbc74d1c26915b4ce447a7
Hwang Ji Hun <hhhhicode@gmail.com>
2022-11-22 17:05:44 +0900

    First Commit

---
+++ src/main/java/HwangJiHun/errorcenter/ErrorcenterApplication.java
@@ -0,0 +1,13 @@
+package HwangJiHun.errorcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorcenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrorcenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/HwangJiHun/errorcenter/ErrorcenterApplicationTests.java
@@ -0,0 +1,13 @@
+package HwangJiHun.errorcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorcenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
